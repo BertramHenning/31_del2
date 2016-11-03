5d6b06732a656716de610148b1ace257c492b8cc
Bertram <Bertram@Bertram-HP>
2016-11-03 21:23:08 +0100

    Added Player Class

--- src/aGameOfGold/Player.java
+++ src/aGameOfGold/Player.java
@@ -1,5 +1,48 @@
 package aGameOfGold;
 
-public class Player {
+public class player {
+
+	private int current, win = 0;
+	private String name;
+
+	private Account points;
+
+	public player(String a) {
+		name = a;
+		points = new Account();
+	}
+
+	// Sets win to 1 if the player has reached the victory condition of 3000 points
+	public void checkWin() {
+		if (points.getGoldcoins() >= 3000){
+			win = 1;
+		}
+	}
+
+	// Adds score to Account object
+	public void addPoints(int a) {
+		points.addgoldCoins(a);
+	}
+
+	// Gets score from Account object
+	public int getPoints() {
+		return points.getGoldcoins();
+	}
+
+	public int getWin() {
+		return win;
+	}
+
+	public int getCurrent() {
+		return current;
+	}
+
+	public void setCurrent(int current) {
+		this.current = current;
+	}
+
+	public String toString() {
+		return name;
+	}
 
 }
