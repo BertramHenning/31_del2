df6ce5a045a8b5e0741f42c1d8343803e56e1e2a
Bertram <Bertram@Bertram-HP.eduroam.wireless.dtu.dk>
2016-11-04 11:11:54 +0100

    jUnit test for Player class

---
+++ src/aGameOfGold/PlayerTest.java
@@ -0,0 +1,31 @@
+package aGameOfGold;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PlayerTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testCheckWin() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddPoints() {
+		fail("Not yet implemented");
+	}
+	
+	
+
+}
